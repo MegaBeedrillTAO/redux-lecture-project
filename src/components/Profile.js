9d4f96bcb37c9dd55be3faf107754ed7aa359ba6
MegaBeedrillTAO <jakekremling@yahoo.com>
2019-09-09 12:18:34 -0500

    Did the stuff

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,36 @@
 import React, {Component} from 'react';
+import store, {UPDATE_SHOW} from '../store';
+
 
 export default class Profile extends Component {
+    constructor(){
+        super();
+        this.state = {
+            favShow: ''
+        }
+    }
+    componentDidMount(){
+        store.subscribe(() => {
+            this.setState({favShow: store.getState().favShow})
+        })
+    }
+    handleChange = e => {
+        let action = {
+            type: UPDATE_SHOW,
+            payload: e.target.value
+        }
+        store.dispatch(action);
+    }
     render() {
         return(
             <div>
-                <input placeholder="Favorite Show" />
+                <input placeholder="Favorite Show" onChange={this.handleChange}/>
                 <input placeholder="Favorite Movie" />
                 <input placeholder="Favorite Color" />
                 <input placeholder="Favorite Food" />
                 <input placeholder="Favorite Band" />
+                <h1>Welcome back, {store.getState().username}</h1>
+                <h2>Favorite Movie: {this.state.favShow}</h2>
             </div>
         )
     }
